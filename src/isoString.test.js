dbd823183a50b9163dd50a963ccd4c9e27e4c553
Lucian S <lucian.stoica@ownzones.com>
2020-06-02 15:55:18 +0300

    corrected startOfDay

--- src/isoString.test.js
+++ src/isoString.test.js
@@ -1,13 +1,21 @@
 const moment = require('moment');
+const startOfDay = require('date-fns/startOfDay');
 moment.locale('en-gb');
 
 describe('ISO string tests', () => {
-  test("startOf('day').toISOString()", () => {
-    const isoString =
-      'Wed Jun 11 2020 00:00:00 GMT+0300 (Eastern European Summer Time)';
-    const toIsoDate = new Date(isoString);
-    const m = moment(toIsoDate).startOf('day').toISOString();
+  const dates = [
+    'Wed Jun 11 2020 00:00:00 GMT+0300 (Eastern European Summer Time)',
+    'Tue Jun 02 2020 15:48:17 GMT+0300 (Eastern European Summer Time)',
+    '05 October 2011 14:48 UTC',
+    '01 October 2011 00:00 UTC',
+  ];
 
-    expect(m).toEqual(toIsoDate.toISOString());
+  test("startOf('day').toISOString()", () => {
+    dates.forEach((date) => {
+      const toIsoDate = new Date(date);
+      const m = moment(toIsoDate).startOf('day').toISOString();
+
+      expect(m).toEqual(startOfDay(toIsoDate).toISOString());
+    });
   });
 });
