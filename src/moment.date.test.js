a4baddf26d1695f508ff989d5730d55b3c8ce91d
Lucian S <lucian.stoica@ownzones.com>
2020-06-25 14:13:35 +0300

    remove tests

--- src/moment.date.test.js
+++
@@ -1,9 +0,0 @@
-const moment = require('moment');
-
-describe('testing moment and Date', () => {
-  test('moment constructor ', () => {
-    const m = moment();
-    const d = new Date();
-    expect(m.day()).toEqual(d.getDay());
-  });
-});
