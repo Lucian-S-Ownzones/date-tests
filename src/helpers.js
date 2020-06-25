98eea82d1c3a87d1b034a0697ea9791b8382fc19
Lucian S <lucian.stoica@ownzones.com>
2020-06-25 14:13:08 +0300

    try-catch helper

--- src/helpers.js
+++ src/helpers.js
@@ -2,12 +2,13 @@ const format = require('date-fns/format');
 const parseISO = require('date-fns/parseISO');
 
 const dateFormatter = (date, ...args) => {
-  let d = date instanceof Date ? date : parseISO(date);
-  if (!(d instanceof Date) || d.toString() === 'Invalid Date') {
+  try {
+    const d = parseISO(date);
+    if (d.toString() !== 'Invalid Date') return format(d, ...args);
     return d.toString();
+  } catch (error) {
+    return error.message;
   }
-
-  return format(d, ...args);
 };
 
 module.exports = {
