8948043e41ca727f273470cfec47743040aa9dba
Lucian S <lucian.stoica@ownzones.com>
2020-06-26 12:45:03 +0300

    corrected helper

--- src/helpers.js
+++ src/helpers.js
@@ -6,7 +6,7 @@ const parseISO = require('date-fns/parseISO');
 const dateFormatter = (date, formatString, options = { locale: enGB }) => {
   const inputIsDate = date instanceof Date;
   let d = inputIsDate ? date : parseISO(date);
-  if (!inputIsDate || d.toString() === 'Invalid Date') {
+  if (!d || d.toString() === 'Invalid Date') {
     return d.toString();
   }
 
