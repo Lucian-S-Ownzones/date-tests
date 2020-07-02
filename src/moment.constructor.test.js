d08cfb76ed7b99e3894fcf8699a75982119c5ef0
Lucian S <lucian.stoica@ownzones.com>
2020-07-02 12:10:47 +0300

    cleanup

--- src/moment.constructor.test.js
+++ src/moment.constructor.test.js
@@ -1,7 +1,4 @@
 const moment = require('moment');
-const startOfSecond = require('date-fns/startOfSecond');
-const parseISO = require('date-fns/parseISO');
-const format = require('date-fns/format');
 
 describe('moment constructor ', () => {
   test('the moment constructor ', () => {
@@ -9,6 +6,6 @@ describe('moment constructor ', () => {
     const d = new Date();
     expect(d instanceof Date).toBeTruthy();
     expect(m instanceof moment).toBeTruthy();
-    expect(m.toString()).toEqual(format(d, 'pppp'));
+    expect(m instanceof Date).toBeFalsy();
   });
 });
