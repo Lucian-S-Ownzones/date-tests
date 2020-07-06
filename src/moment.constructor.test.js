849449cf1e9184cd9edf6b160cecb51e2bc507cf
Lucian S <lucian.stoica@ownzones.com>
2020-07-06 15:45:50 +0300

    moment constructor vs new Date() checks

--- src/moment.constructor.test.js
+++ src/moment.constructor.test.js
@@ -8,4 +8,15 @@ describe('moment constructor ', () => {
     expect(m instanceof moment).toBeTruthy();
     expect(m instanceof Date).toBeFalsy();
   });
+  test('new Date() gets the same year/month/day HH:mm as moment', () => {
+    // second is hard to test as they will run at slightly diff times
+    const input = moment();
+    const output = new Date();
+
+    expect(input.day()).toEqual(output.getDay());
+    expect(input.month()).toEqual(output.getMonth());
+    expect(input.year()).toEqual(output.getFullYear());
+    expect(input.hour()).toEqual(output.getHours());
+    expect(input.minute()).toEqual(output.getMinutes());
+  });
 });
