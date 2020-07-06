80eb4987b77317b594103bb60a7e96020ec6e001
Lucian S <lucian.stoica@ownzones.com>
2020-07-06 13:03:11 +0300

    isoString update

--- src/isoString.test.js
+++ src/isoString.test.js
@@ -1,12 +1,16 @@
 const moment = require('moment');
 const startOfDay = require('date-fns/startOfDay');
+const { formatISO, endOfDay } = require('date-fns');
 
 moment.locale('en-gb');
 
-const formatToISO = (date) => {
+const formatToISO = (fn) => (date) => {
   const toIsoDate = new Date(date);
-  return startOfDay(toIsoDate).toISOString();
+  return fn(toIsoDate).toISOString();
 };
+
+const startOfDayToISO = formatToISO(startOfDay);
+const endOfDayToISO = formatToISO(endOfDay);
 
 describe('ISO string tests', () => {
   const dates = [
@@ -14,27 +18,41 @@ describe('ISO string tests', () => {
     'Tue Jun 02 2020 15:48:17 GMT+0300 (Eastern European Summer Time)',
     '05 October 2011 14:48 UTC',
     '01 October 2011 00:00 UTC',
+    'Wed Jul 22 2020 00:00:00 GMT+0300 (Eastern European Summer Time)',
   ];
 
-  const isoDatesOutput = ['2020-06-10T21:00:00.000Z'];
-
-  test("startOf('day').toISOString()", () => {
+  test("moment(<date>).startOf('day').toISOString() === startOfDayToISO  ", () => {
     dates.forEach((date) => {
       const toIsoDate = new Date(date);
       const m = moment(toIsoDate).startOf('day').toISOString();
-      const output = formatToISO(date);
+      const output = startOfDayToISO(date);
 
       expect(m).toEqual(output);
     });
   });
 
-  test('startOfDay iso formatting ', () => {
-    const input = formatToISO(dates[0]);
-    const output = '2020-06-10T21:00:00.000Z';
+  test("moment(<date>).startOf('day').toISOString() === endOfDayToISO  ", () => {
+    dates.forEach((date) => {
+      const toIsoDate = new Date(date);
+      const m = moment(toIsoDate).endOf('day').toISOString();
+      const output = endOfDayToISO(date);
 
-    const input1 = formatToISO(dates[2]);
-    const output1 = '2011-10-04T21:00:00.000Z';
-    expect(input).toEqual(output);
-    expect(input1).toEqual(output1);
+      expect(m).toEqual(output);
+    });
+  });
+
+  describe('startOfDayToISO to string formatting check', () => {
+    test('format "dates[0]"', () => {
+      const input = startOfDayToISO(dates[0]);
+      const output = '2020-06-10T21:00:00.000Z';
+
+      expect(input).toEqual(output);
+    });
+
+    test('format "dates[2]"', () => {
+      const input1 = startOfDayToISO(dates[2]);
+      const output1 = '2011-10-04T21:00:00.000Z';
+      expect(input1).toEqual(output1);
+    });
   });
 });
