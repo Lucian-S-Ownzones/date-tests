bd1438b9cee72ecc620c6f2d170a398a7bb8ca19
Lucian S <lucian.stoica@ownzones.com>
2020-11-27 18:05:16 +0200

    udpate diff tests

--- src/various.replace.test.js
+++ src/various.replace.test.js
@@ -1,5 +1,6 @@
 const moment = require('moment');
 const parseISO = require('date-fns/parseISO');
+const isValid = require('date-fns/isValid');
 const { parseISO2 } = require('./helper');
 const parse = require('date-fns/parse');
 const format = require('date-fns/format');
@@ -14,10 +15,11 @@ const {
   getMinutes,
   setHours,
   setMinutes,
+  intervalToDuration,
 } = require('date-fns');
 const add = require('date-fns/add');
-const sub = require('date-fns/sub');
 const _flow = require('lodash/flow');
+const { formatDate } = require('./helper');
 
 const starOfDay = require('date-fns/startOfDay');
 
@@ -34,7 +36,16 @@ const getDateDiff = (start, end) => {
   };
 };
 
-describe('moment constructor', () => {
+const getDuration = (start, end) => {
+  const { hours, minutes, seconds } = intervalToDuration({
+    start: parseISO(start),
+    end: parseISO(end),
+  });
+
+  return { hours, minutes, seconds };
+};
+
+describe('Run this with Havana timezone!!! "test:tz:havana"', () => {
   test('moment and new Date() return same nr of ms (unix based)', () => {
     const m = moment();
     const d = new Date();
@@ -88,15 +99,35 @@ describe('moment constructor', () => {
     const timeStr = `${hoursM}h${minutesM}m${secondsM}s`;
 
     const { hours, minutes, seconds } = getDateDiff(start, end);
+    const { hours: h, minutes: m, seconds: s } = getDuration(start, end);
 
     const hoursD = parseInt(hours);
     const minutesD = parseInt(minutes) % 60;
     const secondsD = parseInt(seconds) % 60;
 
     const timeStrD = `${hoursD}h${minutesD}m${secondsD}s`;
+    const timeStrD2 = `${h}h${m}m${s}s`;
 
     expect(timeStr).toEqual('45h24m9s');
     expect(timeStr).toEqual(timeStrD);
+    expect(timeStrD2).toEqual('21h24m9s');
+  });
+
+  test('test date diff with Duration', () => {
+    const start = '2020-09-01T01:00:00.000Z';
+    const end = '2020-09-09T02:00:00.000Z';
+    const { hours, minutes, seconds } = getDateDiff(start, end);
+    const { hours: h, minutes: m, seconds: s } = getDuration(start, end);
+
+    const hoursD = parseInt(hours);
+    const minutesD = parseInt(minutes) % 60;
+    const secondsD = parseInt(seconds) % 60;
+
+    const timeStrD = `${hoursD}h${minutesD}m${secondsD}s`;
+    const timeStrD2 = `${h}h${m}m${s}s`;
+
+    expect(timeStrD).toEqual('193h0m0s');
+    expect(timeStrD2).toEqual('1h0m0s');
   });
 
   test('should have the offset based on the timezone', () => {
@@ -195,7 +226,7 @@ describe('current moment duration', () => {
 
 describe('Notifications| ScheduleDateTime | change', () => {
   const dateRef = '2020-11-13T16:35:27+02:00';
-  const dateHavana = '2020-11-13T14:35:00.000Z';
+  const dateHavana = '2020-11-13T14:35:00.100Z';
 
   const date = new Date(2020, 10, 12, 16, 35);
   const inputValue = '2020-11-13T14:35:00.000Z';
@@ -254,5 +285,108 @@ describe('Notifications| ScheduleDateTime | change', () => {
     const r2 = handleDateChangeD(date, inputValue);
 
     expect(r1).toEqual(r2);
+  });
+  test('get Seconds/Miliseconds ', () => {
+    const refDate = new Date(2020, 1, 1, 10, 1, 2, 331);
+
+    const mMili = moment(refDate).millisecond();
+    const mMilis = moment(refDate).milliseconds();
+
+    expect(mMili).toEqual(331);
+    expect(mMilis).toEqual(331);
+    expect(refDate.getMilliseconds()).toEqual(331);
+
+    expect(moment(refDate).seconds()).toEqual(2);
+    expect(refDate.getSeconds()).toEqual(2);
+
+    const dateHavana = '2020-11-13T14:35:00.100Z';
+
+    expect(moment(dateHavana).millisecond()).toEqual(100);
+    expect(moment(dateHavana).seconds()).toEqual(0);
+
+    expect(parseISO2(dateHavana).getMilliseconds()).toEqual(100);
+    expect(parseISO2(dateHavana).getSeconds()).toEqual(0);
+
+    const dateHavanaC = new Date(2020, 11, 13, 9, 35, 0, 100);
+    const dateHavanaC_1 = '2020-12-13T14:35:00.100Z';
+    expect(dateHavanaC.getMonth()).toEqual(11);
+    expect(dateHavanaC.getDay()).toEqual(0);
+
+    expect(parseISO2(dateHavanaC_1).getMonth()).toEqual(11);
+    expect(parseISO2(dateHavanaC_1).getDay()).toEqual(0);
+
+    expect(moment(dateHavanaC_1).format('HH:mm')).toEqual('09:35');
+    expect(format(parseISO(dateHavanaC_1), 'HH:mm')).toEqual('09:35');
+    expect(format(dateHavanaC, 'HH:mm')).toEqual('09:35');
+  });
+  test('checking HH:mm for strings like ', () => {
+    const ref = {
+      interval: 'daily',
+      time: '17:41',
+      timezone: 'America/Havana',
+    };
+
+    const time = moment(ref.time, 'HH:mm').format('HH:mm');
+    expect(time).toEqual('17:41');
+
+    const time2 = moment('01:02', 'HH:mm').format('HH:mm');
+    expect(time2).toEqual('01:02');
+  });
+
+  test('utc parsing date', () => {
+    const utc1 = moment().utc().toISOString();
+    const mf = moment().format();
+    const utc2 = moment().utc().format();
+    expect(mf.substr(-6)).toEqual('-05:00');
+    expect(utc2.substr(-6)).not.toEqual('-05:00');
+    const nowToISO = () => {
+      const d = new Date();
+      const dStr = d.toISOString();
+      return `${dStr.substr(0, dStr.indexOf('.'))}Z`;
+    };
+
+    const nowToISO2 = () => {
+      const d = new Date();
+      return d.toISOString();
+    };
+
+    expect(utc2).toEqual(nowToISO());
+    // expect(utc1).toEqual(nowToISO2());
+  });
+
+  test('moment().format() equivalence formatISO(new Date())', () => {
+    const d = new Date(2020, 1, 1, 10, 11);
+
+    expect(moment(d).format()).toEqual('2020-02-01T10:11:00-05:00');
+    expect(formatISO(d)).toEqual('2020-02-01T10:11:00-05:00');
+    expect(moment(d).format('HH:mm')).toEqual('10:11');
+    expect(format(d, 'HH:mm')).toEqual('10:11');
+  });
+
+  test('date evaluation and format', () => {
+    const mD = (d) => {
+      const m = moment(d);
+      return (m.isValid() ? m : moment()).format('DDMMYYYY_HH:mm');
+    };
+
+    const dfD = (d) => {
+      return (
+        formatDate(d, 'ddMMyyyy_HH:mm') ||
+        formatDate(new Date(), 'ddMMyyyy_HH:mm')
+      );
+    };
+
+    const validDates = [
+      '2020-01-21T11:52:04.197Z',
+      '2019-11-04T08:51:49.501Z',
+      '2019-08-21T15:05:34.482Z',
+    ];
+    const invalidDate = ['x019-30-33T15:05:34.482Z'];
+
+    validDates.forEach((date) => {
+      expect(dfD(date)).toEqual(mD(date));
+    });
+
+    // expect(mD(invalidDate)).toEqual(dfD(invalidDate));
   });
 });
